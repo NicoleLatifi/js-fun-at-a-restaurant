038ba0b29f2ca79a91fc357bbcb5206d868d3d7b
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-26 23:14:12 -0700

    Write functions to pass all tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,28 @@
+function takeOrder(orderNumber, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+    return deliveryOrders.push(orderNumber)
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  return deliveryOrders.splice((orderNumber - 1), 1)
+}
+
+function listItems(orders) {
+  var results = ""
+  for(let i = 0; i < orders.length; i++) {
+    results = results + orders[i].item + ", "
+  }
+  return results.substring(0, results.length - 2)
+}
+
+function searchOrder(orders, name) {
+  return listItems(orders).includes(name)
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
