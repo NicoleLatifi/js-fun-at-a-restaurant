2f8b306cc3cdd639283f765ea0c95c5b4c340535
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-26 21:11:30 -0700

    Write functions to pass all tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+}
+
+function addIngredients(name, ingredients) {
+  if(!ingredients.includes(name)) {
+    return ingredients.push(name)
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price = price * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
 }
 
 
